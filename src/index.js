5695c3c33dbb9690af0e81d234fcdcff8bd7fab8
Denys-Zabolotskyi <70952705+Denys-Zabolotskyi@users.noreply.github.com>
2022-08-11 22:48:18 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
-test;
+
+const test = fetch('https://restcountries.com/v3.1/all');
+
+//   .then(data => {
+//     console.log(data.common);
+//   });
+// console.log(test);
